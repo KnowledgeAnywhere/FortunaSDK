61cabd06d608a92fac7dd59aa8a069c6011a7e9a
Taylor Ackley <taylorackley@gmail.com>
2018-02-08 21:02:46 -0800

    doc changes

--- index.js
+++ index.js
@@ -14,34 +14,8 @@ const Fortuna = {};
 // Undocumented Module
 Fortuna.$ = FortunaAPI;
 
-/**
- * @name Fortuna#Ping
- * @see Ping
- * @alias fortuna.ping
- */
 Fortuna.ping = Ping;
-
-/**
- * @name Fortuna#Auth
- * @see {@link module: Auth}
- * @alias fortuna.auth
- */
 Fortuna.auth = Auth;
-
-/**
- * @name Fortuna#User
- * @see {@link module: User}
- * @alias fortuna.user
- */
 Fortuna.user = User;
-
-/**
- * @name Fortuna#Users
- * @see {@link module: Users}
- * @alias fortuna.user
- */
-
 Fortuna.users = Users;
-
-
 module.exports = Fortuna;
