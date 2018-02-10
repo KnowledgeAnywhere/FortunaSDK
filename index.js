f12677031d63e9a1b9d14862923f42e1ebb264b9
Taylor Ackley <taylorackley@gmail.com>
2018-02-09 21:13:49 -0800

    pushing towards 1.0

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ const FortunaAPI = require('./lib/modules/fortuna-api.module.js');
 const Ping = require('./lib/modules/ping.module');
 const Auth = require('./lib/modules/auth.module');
 const User = require('./lib/modules/user.module');
-const Users = require('./lib/modules/users.module');
-
-/**
- * @namespace {object} Fortuna
- * @description Top-Level namespace for the Fortuna SDK.
- */
 
 const Fortuna = {};
 
-// Undocumented Module
-Fortuna.$ = FortunaAPI;
-
+Fortuna.api = FortunaAPI;
 Fortuna.ping = Ping;
 Fortuna.auth = Auth;
 Fortuna.user = User;
-Fortuna.users = Users;
 module.exports = Fortuna;
